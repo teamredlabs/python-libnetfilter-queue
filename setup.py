7bdb7cf321ef8386bae7ed5196c6d9de7cc93e75
John Lawrence M. Penafiel <penafieljlm@gmail.com>
2026-09-16 21:27:45 +0800

    Implement type stubs for libnetfilterqueue module

--- setup.py
+++ setup.py
@@ -1,6 +1,23 @@
 """The setup.py script."""
 
-from distutils.core import setup, Extension
+import os
+
+from setuptools import setup, Extension
+from setuptools.command.build_py import build_py
+
+
+class libnetfilter_build_py(build_py):
+
+    def run(self):
+        build_py.run(self)
+        dest = os.path.join(
+            self.build_lib,
+            'libnetfilterqueue-stubs',
+            '__init__.pyi',
+        )
+        self.mkpath(os.path.dirname(dest))
+        self.copy_file('libnetfilterqueue.pyi', dest)
+
 
 setup(name="python-libnetfilter-queue",
       version='0.0.1',
@@ -22,8 +39,9 @@ setup(name="python-libnetfilter-queue",
                    'Topic :: Internet :: Log Analysis',
                    'Topic :: System :: Networking :: Monitoring'],
       keywords='libnetfilter libnetfilterqueue netfilter nfqueue',
-      ext_modules=[Extension(
-          name="libnetfilterqueue",
-          sources=["libnetfilterqueue.c"],
-          libraries=["netfilter_queue", "nfnetlink"]
-      )])
+      ext_modules=[Extension(name="libnetfilterqueue",
+                             sources=["libnetfilterqueue.c"],
+                             libraries=["netfilter_queue", "nfnetlink"])],
+      cmdclass={'build_py': libnetfilter_build_py},
+      packages=['libnetfilterqueue-stubs'],
+      zip_safe=False)
